215d5b35141e7d7cb47948769023851d5993b7f7
AnastasiiaSukha <suha.anastasia@gmail.com>
2022-05-15 19:12:38 +0400

    Updated task 03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,48 @@
+
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formEl = document.querySelector('.form');
+formEl.addEventListener('click', onSubmintForm);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+    resolve({ position, delay });
   } else {
-    // Reject
+    reject({ position, delay });
   }
+    }, delay)
+  
+  })
+};
+
+function onSubmintForm(event) {
+  event.preventDefault();
+
+  const delay = Number(formEl.delay.value);
+  const step = Number(formEl.step.value);
+  const amount = Number(formEl.amount.value);
+  let delayStart = delay;
+
+  if (delayStart <= 0) {
+    return;
+  }
+  for (let i = 0; i < amount; i += 1) {
+    let countStart = i + 1;
+  
+  createPromise(countStart, delayStart)
+  .then(({ position, delay }) => {
+    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, {
+      clickToClose: true,
+    });
+  })
+  .catch(({ position, delay }) => {
+    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, {
+      clickToClose: true,
+    });
+  });
+    delayStart += step;
+    };
 }
